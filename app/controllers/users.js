1855e4bcb461de2469f184b38c4c132846dc0b49
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2015-09-22 17:04:01 +0200

    implemented users list

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,7 +3,20 @@ var logger = require('../utils/logger');
 function UsersCtrl() {
     var me = this;
 
-    me.list = function () {};
+    me.list = function (req, res) {
+        logger.info('UsersCtrl:list');
+        
+        res.json([{
+            id: 10,
+            name: 'Wilk',
+            email: 'vincenzo@ibuildings.it'
+        }, {
+            id: 20,
+            name: 'foo',
+            email: 'foo@foo.com'
+        }]);
+    };
+
     me.single = function () {};
     me.create = function () {};
     me.update = function () {};
