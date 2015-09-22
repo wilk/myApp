cc62bbf29b5ed2bd87bb184de4922c58f2bf1110
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2015-09-22 16:59:45 +0200

    implemented get single user

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -4,7 +4,18 @@ function UsersCtrl() {
     var me = this;
 
     me.list = function () {};
-    me.single = function () {};
+
+    me.single = function (req, res) {
+        var user = req.params.user;
+        logger.info('UsersCtrl::single');
+
+        res.json({
+            id: user,
+            name: 'Wilk',
+            email: 'vincenzo@ibuildings.it'
+        });
+    };
+
     me.create = function () {};
     me.update = function () {};
     me.delete = function () {};
